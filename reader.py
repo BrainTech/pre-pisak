d6efa5972ae66d617d69d844f1515a4807b7615a
Jerzy Grynczewski <jgrynczewski@student.uw.edu.pl>
2014-07-10 22:07:19 +0200

    Change name refs #418

--- reader.py
+++ reader.py
@@ -1,20 +1,20 @@
 #!/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-# This file is part of AT-Platform.
+# This file is part of Pre-Pisak.
 #
-# AT-Platform is free software: you can redistribute it and/or modify
+# Pre-Pisak is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# AT-Platform is distributed in the hope that it will be useful,
+# Pre-Pisak is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with AT-Platform. If not, see <http://www.gnu.org/licenses/>.
+# along with Pre-Pisak. If not, see <http://www.gnu.org/licenses/>.
 
 class reader():
     def __init__(self):
@@ -32,5 +32,3 @@ class reader():
     #-------------------------------------------------------------------------
     def getParameters(self):
         return self.parameters
-
-		

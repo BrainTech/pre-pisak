38feaf56e712e6564dc84a93df79bd12583f016a
Jerzy Grynczewski <jgrynczewski@student.uw.edu.pl>
2014-08-24 22:23:50 +0200

    Adding rmvb and mov support

--- modules/movie.py
+++ modules/movie.py
@@ -114,7 +114,7 @@ class movie( wx.Frame ):
 				fileExtension = item[ item.rfind('.')+1: ]
 				if fileExtension == 'png' or fileExtension == 'jpg' or fileExtension == 'jpeg':
 					self.existingLogos.append( item )
-				elif fileExtension == 'mp4' or fileExtension == 'avi' or fileExtension == 'AVI' or fileExtension == 'VOB' or fileExtension == 'mkv':
+				elif fileExtension == 'mp4' or fileExtension == 'avi' or fileExtension == 'AVI' or fileExtension == 'VOB' or fileExtension == 'mkv' or fileExtension == 'mov' or fileExtension == 'MOV' or fileExtension = 'rmvb':
 					self.existingMedia.append( item )
 
 			self.existingLogos = sorted( self.existingLogos, key=lambda name: int(name.split( '_', 2 )[ 0 ]) )
